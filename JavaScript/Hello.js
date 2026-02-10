1be6f0eaf571f9c5a721c15cce20d7a40aa89340
Bhautik Ranpara <ranparabhautik30@gmail.com>
2026-02-10 17:34:45 +0530

    new updated FK

---
+++ JavaScript/Hello.js
@@ -0,0 +1,36 @@
+
+// // console.log(x)
+// getName()
+// // fetchName()
+// x=10
+// // console.log(getName)
+// function getName(){
+//     console.log("Hello World")
+// }
+
+
+// // getName()
+// // console.log(x)
+
+
+// var fetchName = () =>{
+//     console.log("Message from fetchName")
+// }
+// ----------------------------------------------until ep 4--------------------------------------
+
+
+
+var x=1
+a();
+b();
+console.log(x);
+
+function a(){
+   var x=10
+    console.log(x);
+}
+function b(){
+    var x=100
+    console.log(x);
+}
+
